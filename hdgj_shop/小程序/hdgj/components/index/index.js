fbf3da7e8c950c1bb4b87af11f5a0ebacee874b5
handsome-111 <1040978586@qq.com>
2019-07-18 17:41:10 +0800

    2019-07-18

--- hdgj_shop/小程序/hdgj/components/index/index.js
+++ hdgj_shop/小程序/hdgj/components/index/index.js
@@ -24,7 +24,31 @@ Component({
   data: {
     images: {
       url: 'http://img1.imgtn.bdimg.com/it/u=735014917,976731480&fm=26&gp=0.jpg',
-    }
+    },
+    menu:[
+      { src: '/images/index/1.jpg', title: '港视港饮' },
+      { src: '/images/index/1.jpg', title: '港视港饮' },
+      { src: '/images/index/1.jpg', title: '港视港饮' },
+      { src: '/images/index/1.jpg', title: '港视港饮' },
+      { src: '/images/index/1.jpg', title: '港视港饮' },
+      { src: '/images/index/1.jpg', title: '港视港饮' },
+      { src: '/images/index/1.jpg', title: '港视港饮' },
+      { src: '/images/index/1.jpg', title: '港视港饮' }
+    ],
+    template_block:[
+      {
+        title:'每日精选',
+        goods:[
+          {
+            url: '/images/index/1.jpg',
+            title:'美国UASA保健品',
+            explain:'好吃的好喝的好玩的',
+            price:90.5,
+            originPrice:170.10
+          }
+        ]
+      }
+    ]
   },
 
   /**
