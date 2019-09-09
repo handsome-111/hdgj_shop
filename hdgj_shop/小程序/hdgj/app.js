8795aa0c0553c0821f03b391fa80746934855abe
handsome <1040978586@qq.com>
2019-09-09 23:25:02 +0800

    1

--- hdgj_shop/小程序/hdgj/app.js
+++ hdgj_shop/小程序/hdgj/app.js
@@ -5,13 +5,45 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs) 
-    console.log('出发lauch')
-    this.login(this.globalData.userInfo)
+    var app = this
+    var promise = new Promise((resolve,reject) => {
+
+      wx.login({
+        success: function (res) {
+          /** 
+           * 发出请求授权登陆    
+           **/
+          wx.request({
+            url: 'http://127.0.0.1/wxLogin',
+            data: {
+              //用户登录凭证（有效期五分钟）。开发者需要在开发者服务器后台调用 auth.code2Session，使用 code 换取 openid 和 session_key 等信息
+              js_code: res.code,
+              userinfo: null
+            },
+            success: function (response) {
+              var resUser = response.data.userInfo
+              /**
+               * 将用户信息存储在客户端内存中
+               */
+              if (resUser != null) {
+                app.globalData.isLogin = true
+                app.globalData.userInfo = resUser
+              }
+              console.log('-----' + app.globalData.userInfo)
+              resolve()
+            }
+          })
+        }
+      })  
+    })
+
+    promise.then(() => {
+      console.log('---111--' + app.globalData.userInfo)
+    })
+
+    //this.login(this.globalData.userInfo)
 
   }, 
-  onShow:function(){
-    console.log('触发登录')
-  },
   globalData: {  
     userInfo: null,             //用户信息
     serverHost:'localhost',     //服务器地址
@@ -27,32 +59,7 @@ App({
       
     // })
 
-    wx.login({
-      success: function (res) {  
-        /** 
-         * 发出请求授权登陆    
-         **/   
-        wx.request({ 
-          url: 'http://127.0.0.1/wxLogin',    
-          data: {
-            //用户登录凭证（有效期五分钟）。开发者需要在开发者服务器后台调用 auth.code2Session，使用 code 换取 openid 和 session_key 等信息
-            js_code: res.code,  
-            userinfo:userInfo
-          },  
-          success: function (response) {
-            var resUser = response.data.userInfo
-            /**
-             * 将用户信息存储在客户端内存中
-             */
-            if(resUser != null){
-              app.globalData.isLogin = true
-              app.globalData.userInfo = resUser
-            }
-            console.log(app.globalData.userInfo) 
-          }  
-        })    
-      }   
-    })     
+       
   },
   /** 
    * checkSession 
@@ -63,7 +70,7 @@ App({
  
       },  
       fail: () => {
-        app.login();
+        app.login(app.globalData.userInfo);
       }
     })
   },
