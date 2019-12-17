13bafa1338ee772dcd821e57136ba2acedffcd58
handsome-111 <1040978586@qq.com>
2019-12-17 17:06:36 +0800

    1

---
+++ solr/src/main/java/com/hdgj/solr/SolrApplication.java
@@ -0,0 +1,12 @@
+package com.hdgj.solr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SolrApplication.class, args);
+	}
+
+}
